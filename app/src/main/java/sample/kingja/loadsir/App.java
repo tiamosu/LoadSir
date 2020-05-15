016ee78e8c155a02b7aaec66872445f9b380c8c3
djy <137359049@qq.com>
2020-05-15 15:08:21 +0800

    1.LeakCanary 移除相关配置，无需手动配置

--- app/src/main/java/sample/kingja/loadsir/App.java
+++ app/src/main/java/sample/kingja/loadsir/App.java
@@ -3,7 +3,6 @@ package sample.kingja.loadsir;
 import android.app.Application;
 
 import com.kingja.loadsir.core.LoadSir;
-import com.squareup.leakcanary.LeakCanary;
 
 import sample.kingja.loadsir.callback.CustomCallback;
 import sample.kingja.loadsir.callback.EmptyCallback;
@@ -17,14 +16,10 @@ import sample.kingja.loadsir.callback.TimeoutCallback;
  * Author:KingJA
  * Email:kingjavip@gmail.com
  */
-
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (setupLeakCanary()) {
-            return;
-        }
 
         LoadSir.beginBuilder()
                 .addCallback(new ErrorCallback())
@@ -34,15 +29,5 @@ public class App extends Application {
                 .addCallback(new CustomCallback())
                 .setDefaultCallback(LoadingCallback.class)
                 .commit();
-    }
-
-    private boolean setupLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return true;
-        }
-        LeakCanary.install(this);
-        return false;
     }
 }
